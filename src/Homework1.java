46a88da543a34d67598bd13c86309102ccefeca0
Jackaren22 <Jackaren22@gmail.com>
2019-09-15 12:38:56 +0200

    Update Homework1.java

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,8 +1,26 @@
+import java.util.Scanner; // behövs om man vill ha input
 
 public class homework1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+        Scanner input = new Scanner(System.in); // behövs om man vill ha input
+		
+		String namn1 = "Tomas Weis";
+		String namn2 = "Andreas Ohlsson";
+		String namn3 = "Zachris Markström Stenhammar";
+		
+		System.out.println("Skriv ett namn: "); // skriver ut ett meddelande
+		
+		String namn4 = input.nextLine(); // läser in ett namn från konsolen
+
+		System.out.println("Skriv ett namn: ");
+		
+		String namn5 = input.nextLine();
+		
+		System.out.println("\n\tNAMN:");
+		System.out.println(namn1 + "\n" + namn2 + "\n" + namn3 + "\n" + namn4 + "\n" + namn5);
 
 	}
 
