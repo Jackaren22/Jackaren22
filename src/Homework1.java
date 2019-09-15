6c16a65aefc4a8a491cd317e91db55c48f7852c8
Jackaren22 <Jackaren22@gmail.com>
2019-09-15 12:42:17 +0200

    Update Homework1.java

--- src/Homework1.java
+++ src/Homework1.java
@@ -7,20 +7,16 @@ public class homework1 {
 		
         Scanner input = new Scanner(System.in); // behövs om man vill ha input
 		
-		String namn1 = "Tomas Weis";
-		String namn2 = "Andreas Ohlsson";
-		String namn3 = "Zachris Markström Stenhammar";
+		System.out.println("What is your name? "); // skriver ut ett meddelande
 		
-		System.out.println("Skriv ett namn: "); // skriver ut ett meddelande
-		
-		String namn4 = input.nextLine(); // läser in ett namn från konsolen
+		String name = input.nextLine(); // läser in ett namn från konsolen
 
-		System.out.println("Skriv ett namn: ");
+		System.out.println("How old are you? ");
 		
-		String namn5 = input.nextLine();
+		String age = input.nextLine();
 		
-		System.out.println("\n\tNAMN:");
-		System.out.println(namn1 + "\n" + namn2 + "\n" + namn3 + "\n" + namn4 + "\n" + namn5);
+		System.out.println("\n\tInformation:");
+		System.out.println(name + "\n" + age);
 
 	}
 
