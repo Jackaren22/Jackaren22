270028b876d149881bd93b122f880f9ae770ae31
Jackaren22 <Jackaren22@gmail.com>
2019-09-25 14:43:04 +0200

    Kattis
    Kattis uppgifter

---
+++ kattis/timeloop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class timeloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		        Scanner input = new Scanner(System.in);
+
+		        int num = input.nextInt();
+		        for (int i = 1; i <= num; i++)
+		        	
+		        System.out.println(i + " Abracadabra");
+		       
+		    }
+		
+	}
+
